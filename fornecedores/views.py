3299fdf104d8f493d28e9a872e9c08250af03ca7
Aluno <Aluno@politecnico.local>
2026-03-30 21:31:45 -0300

    fornecedores seed

--- fornecedores/views.py
+++ fornecedores/views.py
@@ -1,3 +1,7 @@
+
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .forms import FornecedorModelForm
@@ -12,21 +16,30 @@ class FornecedoresView(ListView):
         qs = super(FornecedoresView, self).get_queryset()
         if buscar:
             return qs.filter(nome__icontains=buscar)
-        return qs
 
-class FornecedorAddView(CreateView):
+        if qs.count() > 0:
+            paginator = Paginator(qs, 11)
+            listagem = paginator.get_page(self.request.GET.get('page'))
+            return listagem
+        else:
+            return messages.info(self.request, 'Não existem fornecedores cadastrados!')
+
+class FornecedorAddView(SuccessMessageMixin,CreateView):
     model = Fornecedor
     form_class = FornecedorModelForm
     template_name = 'fornecedor_form.html'
     success_url = reverse_lazy('fornecedores')
+    success_message = 'Fornecedor adicionado com sucesso!'
 
-class FornecedorUpdateView(UpdateView):
+class FornecedorUpdateView(SuccessMessageMixin,UpdateView):
     model = Fornecedor
     form_class = FornecedorModelForm
     template_name = 'fornecedor_form.html'
     success_url = reverse_lazy('fornecedores')
+    success_message = 'Fornecedor atualizado com sucesso!'
 
-class FornecedorDeleteView(DeleteView):
+class FornecedorDeleteView(SuccessMessageMixin,DeleteView):
     model = Fornecedor
     template_name = 'fornecedor_apagar.html'
     success_url = reverse_lazy('fornecedores')
+    success_message = 'Fornecedor apagado com sucesso!'
